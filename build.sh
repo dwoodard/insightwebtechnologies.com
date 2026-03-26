26b999f09f2fed1f72dcf037799b1ea2d39c4740
Dustin Woodard <dustin.woodard@gmail.com>
2026-03-25 22:11:39 -0600

    fix: ensure git pull is executed before installation and force migration

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@
 
 # so this script will be run on the server, and it will build the project and then move the built files to the correct location
 
+git pull
+
 php-8.4 ~/bin/composer.phar install
 npm install
 # npm run build # build doesn't work here
@@ -13,4 +15,4 @@ php-8.4 ~/bin/composer.phar dump-autoload -o
 php-8.4 artisan config:cache
 php-8.4 artisan route:cache
 php-8.4 artisan view:cache
-php-8.4 artisan migrate
+php-8.4 artisan migrate --force
