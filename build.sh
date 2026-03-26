f5880839f38fa08d05e944fb91050f2ddad47aed
Dustin Woodard <dustin.woodard@gmail.com>
2026-03-25 20:53:46 -0600

    Add build script for project setup on DreamHost server

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# build.sh
+
+# because we are on dreamhost we don't have access to things easily, so we have to do some work to get things working
+
+# so this script will be run on the server, and it will build the project and then move the built files to the correct location
+
+php-8.4 ~/bin/composer.phar install
+npm install
+# npm run build # build doesn't work here
+
+php-8.4 ~/bin/composer.phar dump-autoload -o
+php-8.4 artisan config:cache
+php-8.4 artisan route:cache
+php-8.4 artisan view:cache
+php-8.4 artisan migrate
